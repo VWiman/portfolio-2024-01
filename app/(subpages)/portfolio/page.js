363d53c2b54da9d4f798e8a5c2c26431220d6966
why_code <119089018+VWiman@users.noreply.github.com>
2024-03-17 13:38:13 +0100

    Added documents

--- app/(subpages)/portfolio/page.js
+++ app/(subpages)/portfolio/page.js
@@ -1,7 +1,12 @@
-export default function portfolio() {
+import Link from "next/link";
+
+export default function Portfolio() {
 	return (
 		<main className="main">
-			<section id="projects" name="projects" className="section">Projects</section>
+			<section id="projects" name="projects" className="section">
+				Projects
+				<Link href="https://portfolio-user-profile-ccovwe4ml-viktor-wimans-projects.vercel.app/">User Profile - Example</Link>
+			</section>
 		</main>
 	);
 }
